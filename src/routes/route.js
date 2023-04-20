da421f3ec104fef138811cba0021d7f979cab4a2
Saurabh Digambar <saurabhdigambar8@gmail.com>
2023-04-20 18:46:32 +0530

    2 question solve

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,52 +1,50 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common')
-const myUnderscore = require('underscore')
 
-router.get('/test-me', function (req, res) {
-    res.send('This should be working!')
-});
+// const commonFile = require('./common')
+// const myUnderscore = require('underscore')
 
-router.get('/test-you', function (req, res) {
-    console.log('The exported module is: ',commonFile)
-    commonFile.doSomething()
-    console.log('This is the constant I created', commonFile.name)
-    res.send('Hello there, welcome to this application!')
-});
+// router.get('/cohorts', function (request, response){
+//     // logic to get the cohorts from database
+//     // logic tp get only the active cohorts
+//     // logic to get only the cohort with a size than 50
+//     // logic to get only the backend cohorts
+//     response.send(['technetium','nobelium'])
+// })
 
-router.get('/test-underscore', function(req, res){
-    let result = myUnderscore.first([11,12,23,44,15], 4)
-    console.log('the result is',result)
-    res.send('done')
+// router.get('/students', function(req, res){
+//     // receive or access the query params in the code
+//     // write a logic on these query params
+//     // city, score
+//     console.log(req.query)
+//     let requestedCity = req.query.city
+//     let sortField = req.query.sort
+//     // logic to get students
+//     res.send(["Sabiha","Neha","Akash","Sonali"])
+// })
+
+// router.get('/students/:studentName', function(req, res) {
+//     console.log(req.params.studentName)
+//     /// go to database and search for studentName student
+//     // store the data found in this variable - studentDetails
+//     //res.send({data: studentDetails})
+//     res.send('student data')
+// })
+let MoviesArr=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+router.get('/GET/Movie',function(req,res){
+    res.send(MoviesArr)
 })
-
-router.get('/cohorts', function (request, response){
-    // logic to get the cohorts from database
-    // logic tp get only the active cohorts
-    // logic to get only the cohort with a size than 50
-    // logic to get only the backend cohorts
-    response.send(['technetium','nobelium'])
+router.get('/GET/movies/:indexNumber',function (req,res){
+    let i=req.params.indexNumber
+    if(i>0&&i<=MoviesArr.length){
+    res.send(MoviesArr[i-1])}
+    else{
+        res.send("No is invalid")
+    }
 })
+router.get()
 
-router.get('/students', function(req, res){
-    // receive or access the query params in the code
-    // write a logic on these query params
-    // city, score
-    console.log(req.query)
-    let requestedCity = req.query.city
-    let sortField = req.query.sort
-    // logic to get students
-    res.send(["Sabiha","Neha","Akash","Sonali"])
-})
-
-router.get('/students/:studentName', function(req, res) {
-    console.log(req.params.studentName)
-    /// go to database and search for studentName student
-    // store the data found in this variable - studentDetails
-    //res.send({data: studentDetails})
-    res.send('student data')
-})
 
 
 module.exports = router;
