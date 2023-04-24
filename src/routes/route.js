ffc492c719180dc3e4c54a06a340edaedeecfa85
Saurabh Digambar <saurabhdigambar8@gmail.com>
2023-04-24 18:49:55 +0530

    solved assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,46 +3,29 @@ const { route } = require('express/lib/application');
 const { param } = require('express/lib/router');
 const router = express.Router();
 
-let MoviesArr=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-router.get('/movie',function(req,res){
-    res.send(MoviesArr)
+let arr=[1,2,3,5,6,7] 
+router.get('/sol1',function(req,res){
+  let sum=7*(7+1)/2 //n(n+1)/2
+  let arrsum=0
+  for(let i=0;i<arr.length;i++){
+    arrsum=arrsum+arr[i]
+  }
+  let missingNum=sum-arrsum 
+    res.send({data:missingNum})
 })
-router.get('/movies/:indexNumber',function (req,res){
-    let i=req.params.indexNumber
-    if(i>=0&&i<MoviesArr.length){
-    res.send(MoviesArr[i])}
-    else{
-        res.send("No is invalid plese enter valid number")
-    }
+//===========2nd Api
+
+ let arr2=[33, 34, 35, 37, 38]
+router.get('/sol2',function(req,res){
+  let sum=6*(33+38)/2 //n(first+last)/2 
+  let arrsum=0
+  for(let i=0;i<arr2.length;i++){
+    arrsum=arrsum+arr2[i]
+  }
+  let missingNum=sum-arrsum 
+    res.send({data:missingNum})
 })
 
-const moviesArrOfObj=[ {
-    id: 1,
-    name: "The Shining"
-   }, {
-    id: 2,
-    name: "Incendies"
-   }, {
-    id: 3,
-    name: "Rang de Basanti"
-   }, {
-    id: 4,
-    name: "Finding Nemo"
-   }]
-   
- router.get('/films',function(req,res){
-    res.send(moviesArrOfObj)
- })
-router.get('/films/:filmId',function(req,res){
-   let i=req.params.filmId
-   for(let j=0;j<moviesArrOfObj.length;j++){
-     if(moviesArrOfObj[j].id==i){
-        res.send(moviesArrOfObj[j])
-     }
-   }
-
- res.send("no movie exist with this id ")
-})
 
 
 module.exports = router;
