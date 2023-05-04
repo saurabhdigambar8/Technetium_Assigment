6d25b88e75f76fc565ae84a50b46d3a8edc21168
Saurabh Digambar <saurabhdigambar8@gmail.com>
2023-05-05 00:33:21 +0530

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+//const { default: mongoose } = require('mongoose');
+const moment=require("moment")
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+//mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+//})
+// .then( () => console.log("MongoDb is connected"))
+// .catch ( err => console.log(err) )
 
 app.use (
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        let currentDate=new Date()
+        let date=currentDate.getDate()+'-'+(currentDate.getMonth()+1)+
+        '-'+currentDate.getFullYear()+"  "+currentDate.getHours()+':'+currentDate.getMinutes()+":"+currentDate.getSeconds()
+        //let b=req.path
+        let ip=req.ip
+        let url=req.originalUrl
+        //let date=moment().format("YYYY-MM-DD, hh:mm:ss a")
+        console.log (date,ip,url);
         next();
   }
   );
